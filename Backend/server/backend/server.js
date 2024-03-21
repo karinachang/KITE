ad0c08a52a2db457def4a2c064574a341017a115
Taylarra <146487590+Taylarra@users.noreply.github.com>
2024-03-21 18:15:14 -0500

    Cleaned up server.js

--- Backend/server/backend/server.js
+++ Backend/server/backend/server.js
@@ -1,21 +1,23 @@
-// CONSTANTS FOR SCRIPTS
-const express = require("express");
-const mysql = require("mysql2");
+//CONSTANTS FOR SCRIPTS
+const express = require('express');
+const mysql = require('mysql2');
 const { v4: uuidv4 } = require('uuid');
-const functions = require("@google-cloud/functions");
+const functions = require('@google-cloud/functions');
 
-// CONSTANTS FOR STORAGE BUCKET
+//CONSTANTS FOR STORAGE BUCKET
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage({
-    projectId: "KITE",
-    keyFilename: "/app/backend/bucketUpload/service-account.json",
+	projectId: 'KITE',
+	keyFilename: '/app/backend/bucketUpload/service-account.json',
 });
 const bucket = storage.bucket('kitebucket');
 const bucketName = 'kitebucket';
-const fileName = 'akite.jpg';
-const filePath = '/app/backend/bucketUpload/akite.jpg';
 
-// CONSTANTS FOR DATABASE
+//Constants for testing upload and download
+const fileName = 'akite.jpg'; //Local file
+const filePath = '/app/backend/bucketUpload/akite.jpg'; //Local file location
+
+//CONSTANTS FOR DATABASE
 const PORT = String(process.env.PORT);
 const HOST = String(process.env.HOST);
 const MYSQLHOST = String(process.env.MYSQLHOST);
@@ -23,217 +25,199 @@ const MYSQLUSER = String(process.env.MYSQLUSER);
 const MYSQLPASS = String(process.env.MYSQLPASS);
 const MYSQLDB = String(process.env.MYSQLDB);
 const MYSQLCOL = String(process.env.MYSQLCOL);
-const DATATEST = "TTL";
-var SQL = "SELECT * FROM storage;";
-var hash = "cfdbf3";
+//Selects database
+var SQL = 'SELECT * FROM storage;'
+
+//Testing commands for MYSQL database
+const DATATEST = 'TTL'; //Test SQL commands
+const hash = '66bc1e'; //dummy code
+
+const DEBUG = true;
 
 const app = express();
 app.use(express.json());
 
-
+//Connect to MYSQL DATABASE
 let connection = mysql.createConnection({
-    host: MYSQLHOST,
-    user: MYSQLUSER,
-    password: MYSQLPASS,
-    database: MYSQLDB,
-    multipleStatements: true
+	host: MYSQLHOST,
+	user: MYSQLUSER,
+	password: MYSQLPASS,
+	database: MYSQLDB,
+	multipleStatements: true
 });
 
-//app.use("/", express.static("frontend"));
-
-
-
-// define the function for file download
-async function downloadFile() {
-    // passing the options
-    const options = {
-        destination: `GCPdownload.txt`,
-    };
-    // download object from Google Cloud Storage bucket
-    await bucket.file(fileName).download(options);
-
-    // [optional] a good log can help you in debugging
-    console.log(
-        "The object " + fileName +
-        " coming from bucket " + `kitebucket` +
-        " has been downloaded to " + `GCPdownload.txt`
-    );
-}
-
-//function for generating 6-digit code using uuidv4
-function create_uuid() {
-    let code = uuidv4();
-    let last6 = code.slice(-6);
-    return last6;
-}
-
-//create sql command to insert into database
+//Creates sql command to insert data into database
 function sqlCommand(data) {
-    let file_info = JSON.parse(data);
-    let command = `INSERT INTO storage (  ${MYSQLCOL}  ) VALUES(  ${file_info} )`;
-    return command;
+	let file_info = JSON.parse(data);
+	let command = `INSERT INTO storage ( ${MYSQLCOL} ) VALUES( ${file_info} )`;
+	return command;
 }
 
-// GET ALL ROWS IN DATABASE
-app.get("/query", function (request, response) {
-    SQL = "SELECT *  FROM storage"
-    connection.query(SQL, [true], (error, results, fields) => {
-        if (error) {
-            console.error(error.message);
-            response.status(500).send("database error");
-        } else {
-            console.log(results);
-            response.send(results);
-        }
-    });  
-})
+//Generates unique code using last 6-digits of uuidv4
+function create_6dCode() {
+	let code = uuidv4();
+	let last6 = code.slice(-6);
+	return last6;
+}
 
-app.get("/bucket", async (request, response) => {
-    try {
-        const options = {
-            destination: `GCPdownload.txt`,
-        };
-        // download object from Google Cloud Storage bucket
-        await bucket.file(fileName).download(options);
+//Downloads a file from gcp bucket
+async function downloadFile() {
+	//passing options
+	const options = {
+		destination: 'GCPdownload.txt',
+	};
+	//download object from GCP storage bucket
+	await bucket.file(fileName).download(options);
 
+	if (DEBUG == True) {
+		console.log(
+			'The object ' + fileName + ' coming from bucket ' + `kitebucket` +
+			' has been downloaded to ' + `GCPdownload.txt`
+		);
+	}
+}
 
-        //const file = bucket.file(fileName);
-        /*
-        bucket.file(fileName).download({
-            destination: path.join(process.cwd(), "downloads", fileName)
-        }, function (err) { });*/
-
-
-        // [optional] a good log can help you in debugging
-        console.log(
-            "The object " + fileName +
-            " coming from bucket " + `kitebucket` +
-            " has been downloaded to " + `GCPdownload.txt`
-        );
-        response.send("file download success!");
-
-    } catch {
-        (console.error);
-        response.send("GCP error");
-    };
-})
-
+//Deletes a GIVEN file from gcp bucket
+//ATTENTION: change to take in a filename
 async function deleteFile() {
-    await bucket.file(fileName).delete();
+	await bucket.file(fileName).delete();
 
-    console.log(`gs://kitebucket/${fileName} deleted`);
+	console.log(`gs://kitebucket/${fileName} deleted`);
 }
 
+//Uploads a GIVEN file into gcp bucket
+//ATTENTION: change to take in filename
 async function uploadFile() {
-    const options = {
-        destination: 'akite.jpg',
-    };
+	const options = {
+		destination: `akite.jpg`,
+	};
 
-    await storage.bucket(bucketName).upload(filePath, options);
-    console.log(`${filePath} uploaded to ${bucketName}`);
+await storage.bucket(bucketName).upload(filePath, options);
+console.log(`${filePath} uploaded to ${bucketName}`);
 }
 
-app.get("/deleteFile", async (request, response) => {
-    try {
-        deleteFile().catch(console.error);
-        response.send(`gs://kitebucket/${fileName} deleted`);
-    } catch {
-        (console.error);
-        response.send("delete error");
-    }
+//PAGES
+//Returns all rows in the database
+app.get("/query", function (request, response) {
+	SQL = 'SELECT * FROM storage'
+	connection.query(SQL, [true], (error, results, fields) => {
+		if (error) {
+			console.error(error.message);
+			response.status(500).send('database error');
+		}
+		else {
+			console.log(results);
+			response.send(results);
+		}
+	});
+	return;
 })
 
-// DELETE PROPER ROWS IN DATABASE AND UPDATE THE DOWNLOAD FUNCTION
+//Deletes rows in database and updates remainingDownloads
 app.get("/update", function (request, response) {
-    if (DATATEST == "TTL") {
-        //SQL = "SELECT timeOfDeath FROM storage;"
-        SQL = (
-            `DELETE FROM storage ` +
-            `WHERE timeOfDeath < NOW() OR remainingDownloads = 1; ` +
+	if (DATATEST == 'TTL') {
+		SQL = (
+			`DELETE FROM storage ` +
+			`WHERE timeOfDeath < NOW() OR remainingDownloads = 1; ` +
 
-            `UPDATE storage ` +
-            `SET remainingDownloads = remainingDownloads - 1 ` +
-            `WHERE hash = '${hash}' AND timeOfDeath > NOW() AND remainingDownloads > 0;`
-        );
-        console.log(SQL)
-    }
-    connection.query(SQL, [true], (error, results, fields) => {
-        if (error) {
-            console.error(error.message);
-            response.status(500).send("database error");
-        } else {
-            console.log(results);
-            response.send(results);
-        }
-    });
+			`UPDATE storage ` +
+			`SET remainingDownloads = remainingDownloads - 1 ` +
+			`WHERE hash = '${hash}' AND timeOfDeath > NOW() AND remainingDownloads > 0;`
+		);
+		console.log(SQL)
+	}
+	//send command to database
+	connection.query(SQL, [true], (error, results, fields) => {
+		if (error) {
+			console.error(error.message);
+			response.status(500).send('database error');
+		}
+		else {
+			console.log(results);
+			response.send(results);
+		}
+	});
+	return;
 })
 
-// DISPLAY THE ROWS THAT NEED TO BE DELETED
-app.get("/delete", function (request, response) {
-    if (DATATEST == "TTL") {
-        //SQL = "DELETE FROM storage WHERE timeOfDeath < NOW() OR remainingDownloads = 0;"
-        SQL = "SELECT *  FROM storage WHERE timeOfDeath < NOW() OR remainingDownloads = 1;"
-    }
-    connection.query(SQL, [true], (error, results, fields) => {
-        if (error) {
-            console.error(error.message);
-            response.status(500).send("database error");
-        } else {
-            console.log(results);
-            response.send(results);
-        }
-    });
+//Deletes a file from gcp storage bucket
+app.get("/deleteFile", async (request, response) => {
+	try {
+		deleteFile().catch(console.error);
+		response.send(`gs://kitebucket/${fileName} deleted`);
+	} catch {
+		console.error;
+		response.send('delete error');
+	}
 })
 
-// ADD A LINE INTO THE DATABASE
+//Adds a line to MYSQL database & uploads file to gcp storage bucket
+//ATTENTION: change to take in metadata
 app.get("/uploadFile", function (request, response) {
-    //generate 6-digit code
-    let code = create_uuid();
-    //TESTING- PRINT OUT 6-DIGIT CODE
-    console.log(code);
-    //store code and metadata into array
-    let arr = ["'"+code+"'", "'2024-08-05 00:00:00'",3,"'testpass1'", "'./getrid.gif'"];
-    //generate sql command
-    let SQL = sqlCommand(JSON.stringify(arr));
+	let code = create_6dCode();
+	//store code and metadata into array
+	let arr = ["'" + code + "'", "'2024-08-05 00:00:00'", 3, "'testpass1'", "'./getrid.gif'"];
+	//generate sql command
+	let SQL = sqlCommand(JSON.stringify(arr));
 
-    uploadFile().catch(console.error);
+	//Upload file
+	uploadFile().catch(console.error);
 
-    connection.query(SQL, [true], (error, results, fields) => {
-        if (error) {
-            console.error(error.message);
-            response.status(500).send("database error");
-        } else {
-            console.log(results);
-            response.send(results);
-        }
-    });
-    return;
+	//Send sql command
+	connection.query(SQL, [true], (error, results, fields) => {
+		if (error) {
+			console.error(error.message);
+			response.status(500).send('database error');
+		}
+		else {
+			console.log(results);
+			response.send(results);
+		}
+	});
+	return;
 });
 
-app.get(`/${hash}`, function (request, response) {
-    if (DATATEST == "TTL") {
-        //SQL = "DELETE FROM storage WHERE timeOfDeath < NOW() OR remainingDownloads = 0;"
-        SQL = `SELECT *  FROM storage WHERE hash = '${hash}';`
-    }
-    connection.query(SQL, [true], (error, results, fields) => {
-        if (error) {
-            console.error(error.message);
-            response.status(500).send("database error");
-        } else {
-            console.log(results);
-            response.send(results);
-        }
-    });
+//Displays information given 6digit code
+app.get('/${}', function (request, response) {
+	if (DATATEST == 'TTL') {
+	SQL = `SELECT * FROM storage WHERE hash = '${hash}';`
+}
+//send sql command
+	connection.query(SQL, [true], (error, results, fields) => {
+		if (error) {
+			console.error(error.message);
+			response.status(500).send('database error');
+		}
+		else {
+			console.log(results);
+			response.send(results);
+		}
+	});
+	return;
 })
-/*
-app.post("/uploadDataTest", function (request, response){
-    console.log(request.body);
-    response.send(request.body);
-    return;
-}); */
 
+//ATTENTION: incomplete
+app.get("/bucket", async (request, response) => {
+	try {
+		const options = {
+			destination: `GCPdownload.txt`,
+		};
+		//download object from gcp storage bucket
+		await bucket.file(fileName).download(options);
 
+		if (DEBUG == True) {
+			console.log(`The object ${fileName} coming from bucket ${bucketName} has 
+			been downloaded to GCPdownload.txt`);
+		}
 
+		response.send('file download success!');
+	}
+	catch {
+		console.error;
+		response.send('GCP error');
+	};
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
